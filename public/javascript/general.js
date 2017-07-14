8868b0eda8a6e8a4fdcce6287cd24178849beda8
Karol Sebesta <Sebesta.karol@gmail.com>
2017-07-14 14:21:59 +0200

    Update general.js

--- public/javascript/general.js
+++ public/javascript/general.js
@@ -1,23 +1,23 @@
 $(document).ready(function($) {	
-	/* 
-	* Call modal when clicking on a row in departures table 
-	*/
+    /* 
+    * Call modal when clicking on a row in departures table 
+    */
     $("#DeparturesTablePlaceholder").on("click", ".clickable-row", function() {
 		var id = $(this).attr('id');
 		$("#DeparturesModal-" + id).modal()
     });
 	
-	/* 
-	* Call modal when clicking on a row in returns table 
-	*/
+    /* 
+     * Call modal when clicking on a row in returns table 
+    */
     $("#ReturnsTablePlaceholder").on("click", ".clickable-row", function() {
 		var id = $(this).attr('id');
 		$("#ReturnsModal-" + id).modal()
     });
 	
     /* 
-	* Set active class when selecting bootstrap navbar menu item 
-	*/
+    * Set active class when selecting bootstrap navbar menu item 
+    */
     $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
 	
 	/* 
@@ -26,18 +26,18 @@ $(document).ready(function($) {
 	$("#departures_submit_button").on("click", function (e) {
 		$('#DeparturesLoading').html("<img src='/images/loader.gif'/>");
 	});
-	
+
 	/* 
 	* Display loader animation when clicking on returns form submit button 
 	*/ 
 	$("#returns_submit_button").on("click", function (e) {
 		$('#ReturnsLoading').html("<img src='/images/loader.gif'/>");
 	});
-	
+
 	/* DESTINATIONS FORM SUBMIT:
 	*  1) Performs input fields verification - (this can be done also on backend in /seach route, 
 	*     but I think it is easier and better to do this on frontend before /search request,
-    *     as we are doing several requests for each airline).
+	*     as we are doing several requests for each airline).
 	*  2) Gets airline codes - from hidden input field, which is filled during autocomple
 	*     operation (after destination "from" field value is selected). As list of airlines
 	*     can change, this way the list is up to date during each flight search.
@@ -56,14 +56,14 @@ $(document).ready(function($) {
 		e.preventDefault();					
 		var form = this;
 		console.log();
-		
+
 		// Get airline codes
 		var airline_codes = $('#departure_airlines').val().split(',');
-		
+
 		var promises = [];
 		InitializeModalPlaceholder("#DeparturesModalPlaceholder");
 		var table = InitializeDeparturesTable();
-		
+
 		// Perform /search/:airline_code requests
 		for (var i = 0; i < airline_codes.length; i++) {
 			var request = $.post("/search/" + airline_codes[i], $(this).serialize(), function( data ) {
@@ -73,22 +73,22 @@ $(document).ready(function($) {
 			});
 			promises.push(request);
 		}
-		
+
 		// When all requests are completed display the table and remove loader animation.
 		$.when.apply(null, promises).done(function(){
 			$('#DeparturesLoading').empty();
 			$('#DeparturesTablePlaceholder').html(table);
 			ResetAirlinesAndAirportsFields();
 		})
-		
+
 		// Reset form input fields.
 		form.reset();
 	});
-	
+
 	/* RETURNS FORM SUBMIT:
 	*  1) Performs input fields verification - (this can be done also on backend in /seach route, 
 	*     but I think it is easier and better to do this on frontend before /search request,
-    *     as we are doing several requests for each airline).
+	*     as we are doing several requests for each airline).
 	*  2) Gets airline codes - from hidden input field, which is filled during autocomple
 	*     operation (after returns "from" field value is selected). As list of airlines
 	*     can change, this way the list is up to date during each flight search.
@@ -100,21 +100,21 @@ $(document).ready(function($) {
 		// Imput fields verification
 		if(FormInputVerification (e, "#ReturnFrom", "#ReturnTo", "#ReturnDate", 
 			"#ReturnsLoading", "#ReturnsTablePlaceholder", "#ReturnsError",
-	        "#return_from_airport_code", "#return_to_airport_code")) {
+		"#return_from_airport_code", "#return_to_airport_code")) {
 			return;
 		}
 		// Prevent page to be reloaded
 		e.preventDefault();			
 		var form = this;
 		console.log();
-		
+
 		// Get airline codes
 		var airline_codes = $('#return_airlines').val().split(',');
-		
+
 		var promises = [];
 		InitializeModalPlaceholder("#ReturnsModalPlaceholder");
 		var table = InitializeReturnsTable();
-		
+
 		// Perform /search/:airline_code requests
 		for (var i = 0; i < airline_codes.length; i++) {
 			var request = $.post("/search/" + airline_codes[i], $(this).serialize(), function( data ) {
@@ -124,30 +124,30 @@ $(document).ready(function($) {
 			});
 			promises.push(request);
 		}
-		
+
 		// When all requests are completed display the table and remove loader animation.
 		$.when.apply(null, promises).done(function(){
 			$('#ReturnsLoading').empty();
 			$('#ReturnsTablePlaceholder').html(table);
 			ResetAirlinesAndAirportsFields();
 		})
-		
+
 		// Reset form input fields.
 		form.reset();
 	});
-	
+
 	/*
 	* Perform Form's Input Fields verification.
 	*/
 	function FormInputVerification (e, FromId, ToId, dateId, LoaderId, TablePlaceHolderId, errorId,
-	                                FromAirportCodeId, ToAirportCodeId) {
+					FromAirportCodeId, ToAirportCodeId) {
 		var from_input_field = $(FromId).val();
 		var to_input_field   = $(ToId).val();
 		var date_input_field = $(dateId).val();
 		var from_airport_code = $(FromAirportCodeId).val();
 		var to_airport_code   = $(ToAirportCodeId).val()
 		console.log(from_airport_code + to_airport_code);
-		
+
 		// Check if any of form's input fields are empty and display error code.
 		if (!from_input_field || !to_input_field || !date_input_field) {
 			e.preventDefault();	
@@ -184,7 +184,7 @@ $(document).ready(function($) {
 			}
 		}
 	};
-	
+
 	/*
 	* Reset below fields on /search request success. 
 	* As we are using AJAX, DOM is not refreshed after request,
@@ -198,7 +198,7 @@ $(document).ready(function($) {
 		$("#departure_airlines").val('');
 		$("#return_airlines").val('');
 	}
-	
+
 	/*
 	* Initialize modal placeholders.
 	*/
